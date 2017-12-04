f11f214254c9bad08400dd92e3da0cbeadbdb60d
Andrey Petrov <andrey.petrov@shazow.net>
2017-12-04 13:24:56 -0500

    vue init with typescript

---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  ETHSCAN_KEY: '"SHT8M9JSGR62U5U7YVFUSTPG41IVR1F7ND"',
+  INFURA_KEY: '"cjyvI4Jnrd0hgw3p2vX1"',
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Select Account</h2>
+    <input type="text" /> <input type="button" value="Add" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+})
+</script>
