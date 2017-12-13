78682a33ed6d97377b9bb17ea98ab2303ddaa038
Andrey Petrov <andrey.petrov@shazow.net>
2017-12-13 12:21:41 -0500

    vue-cli init webpack

---
+++ config/prod.env.js
@@ -0,0 +1,7 @@
+'use strict'
+// These are publically exposed by design
+module.exports = {
+  NODE_ENV: '"production"',
+  ETHSCAN_KEY: '"SHT8M9JSGR62U5U7YVFUSTPG41IVR1F7ND"',
+  INFURA_KEY: '"cjyvI4Jnrd0hgw3p2vX1"',
+}
