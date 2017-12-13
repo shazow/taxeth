6deef7357bc40265818d5b293c45917f41e5a8fc
Andrey Petrov <andrey.petrov@shazow.net>
2017-12-13 12:30:48 -0500

    Add vuex

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    accounts: {},
+    messages: [],
+  },
+  mutations: {
+    addAccount(state, account) {
+      if (state.accounts[account]) return;
+      state.accounts[account] = [];
+    },
+    reset(state) {
+      state.accounts = {};
+    },
+  },
+})
