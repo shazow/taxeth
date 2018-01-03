8e4ee860cb61b9056b7948b2b6f0d537e0ec9e61
Andrey Petrov <andrey.petrov@shazow.net>
2018-01-03 12:16:44 -0500

    deploy, extract css

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0.5em 1em;
-}
-
-h2 {
-  margin: 1em 0 0.5em 0;
-  line-height: 1em;
-}
-form {
-  margin-bottom: 2em;
-}
-.warning {
-  width: 600px;
-  display: block;
-  text-align: center;
-  color: rgba(150, 0, 0, 0.9);
-  border: 1px solid rgba(200, 0, 0, 0.3);
-  background-color:rgba(200, 0, 0, 0.1);
-  margin: 0.5rem;
-  font-weight: bold;
-  font-size: 0.9rem;
-}
-
-.warning h3 {
-  margin: 0.5rem 0rem;
-  color: rgba(255, 0, 0, 0.9);
-}
+@import 'assets/css/style.css';
 </style>
