1b1c57450b207f810bd1264742f32940dd1b04b0
Andrey Petrov <andrey.petrov@shazow.net>
2018-01-05 13:53:10 -0500

    loading tag

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,8 +1,8 @@
 <template>
   <form v-on:submit.prevent="addAccount">
     <h2>Accounts</h2>
-    <ul v-for="(_, account) in $store.state.accounts">
-      <li>{{account}}</li>
+    <ul v-for="(account, addr) in $store.state.accounts">
+      <li :class="{ loading: account.loading }">{{addr}}</li>
     </ul>
     <input type="text" v-model="newAccount" class="address" placeholder="Ethereum Address" />
     <input type="submit" value="Add" v-on:click="addAccount" />
@@ -40,5 +40,18 @@ export default {
 <style scoped>
 input.address {
   width: 32em;
+}
+
+.loading::after {
+  margin-left: 1em;
+  padding: 2px 5px;
+  line-height: 1em;
+  border-radius: 3px;
+  vertical-align: middle;
+  content: "LOADING...";
+  font-size: 10px;
+  font-weight: bold;
+  background: rgba(0, 100, 200, 0.6);
+  color: rgba(255, 255, 255, 0.9);
 }
 </style>
